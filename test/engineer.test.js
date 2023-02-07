d1b988f77312efd4277808f9ba82700c1ded6dc0
Mekael <mekael13.me@gmail.com>
2023-02-07 18:22:20 -0500

    added engineer test js

---
+++ test/engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+    it('should set the github username of the engineer', () => {
+        let testGithub = 'github.com/testuser';
+        let testEngineer = new Engineer('Mike', 1, 'random@email', testGithub);
+        expect(testEngineer.github).toBe(testGithub);
+    });
+
+    it('should get the github username of the engineer', () => {
+        let testGithub = 'github.com/testuser';
+        let testEngineer = new Engineer('Mike', 1, 'random@email', testGithub);
+        expect(testEngineer.getGithub()).toBe(testGithub);
+    });
+
+    it('should get the role of the engineer', () => {
+        let testEngineer = new Engineer('Mike', 1, 'random@email', 'github.com/testuser')
+        expect(testEngineer.getRole()).toBe('Engineer');
+    });
+});
